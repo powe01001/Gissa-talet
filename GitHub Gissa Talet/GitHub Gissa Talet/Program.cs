1fa16d9cbafd0acfbb7b94664231e393cb4ed3f1
Pontus Wernersson <powe01001@utb.vaxjo.se>
2019-10-07 13:56:33 +0200

    Gissa Talet Fullfärdat

---
+++ GitHub Gissa Talet/GitHub Gissa Talet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gissa_Talet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till Gissa Talet");
+            Random slump = new Random();
+            slump.Next(101);
+            int tal = slump.Next(101);
+
+            int försök = 0;
+            int tal1 = -1;
+
+            while (tal1 != tal)
+            {
+                Console.WriteLine("Skriv ett tal mellan 1 och 100: ");
+                String Str1 = Console.ReadLine();
+                tal1 = int.Parse(Str1);
+
+                if (tal1 < tal)
+                {
+                    Console.WriteLine("Fel talet är för lågt");
+                    försök++;
+                    Console.WriteLine("Försök igen");
+                }
+
+                if (tal1 > tal)
+                {
+                    Console.WriteLine("Fel talet är för högt");
+                    försök++;
+                    Console.WriteLine("Försök igen");
+                }
+
+                if (tal1 == tal)
+                {
+                    Console.WriteLine("Det är rätt!");
+                    försök++;
+                    Console.WriteLine("Antal försök: " + försök);
+                }
+            }
+        }
+    }
+}
